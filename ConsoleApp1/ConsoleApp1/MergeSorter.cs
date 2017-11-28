7ad87e1e28f8fb9c49fba522d7d53b1b62151797
Saad Al-Khatib <saad.al-khatib@avanade.com>
2017-11-28 13:05:02 +0800

    First commit

---
+++ ConsoleApp1/ConsoleApp1/MergeSorter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MergeSorter
+    {
+        public static Dictionary<string, int> MergeSort(Dictionary<string, int> numbers)
+        {
+            if (numbers.Count <= 1)
+            {
+                return numbers; //base case
+            }
+
+            var left = new Dictionary<string, int>();
+            var right = new Dictionary<string, int>();
+
+            var i = 0;
+            foreach (KeyValuePair<string, int> element in numbers)
+            {
+                if (i % 2 > 0)
+                {
+                    left.Add(element.Key, element.Value);
+                }
+                else
+                {
+                    right.Add(element.Key, element.Value);
+                }
+                i++;
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+
+            return Merge(left, right);
+        }
+
+        private static Dictionary<string, int> Merge(Dictionary<string, int> left, Dictionary<string, int> right)
+        {
+            var result = new Dictionary<string, int>();
+
+            while (left.Count > 0 && right.Count > 0)
+            {
+                if (left.First().Value >= right.First().Value)
+                {
+                    //we take the first element from the left and then remove it
+                    result.Add(left.First().Key, left.First().Value);
+                    left.Remove(left.First().Key);
+                }
+                else
+                {
+                    result.Add(right.First().Key, right.First().Value);
+                    right.Remove(right.First().Key);
+                }
+            }
+
+            // as soon as one of the sides doesn't have more values
+            while (left.Count > 0)
+            {
+                result.Add(left.First().Key, left.First().Value);
+                left.Remove(left.First().Key);
+            }
+
+            while (right.Count > 0)
+            {
+                result.Add(right.First().Key, right.First().Value);
+                right.Remove(right.First().Key);
+            }
+
+
+
+            return result;
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var url = "http://whattomine.com/coins.json";
+            //var currencyRates = _download_serialized_json_data<RootObject>(url);
+
+
+            var json = download_serialized_json_data2(url); ;
+            JObject rss = JObject.Parse(json);
+
+            Dictionary<string, int> coinProfit = new Dictionary<string, int>();
+
+            List<String> coinsList = new List<string>(new string[]
+          { "Feathercoin", "Nicehash-NeoScrypt", "Nicehash-Equihash",
+              "Altcommunity", "Phoenixcoin", "Zcash", "Zencash", "Hush", "Zclassic", "EthereumClassic",
+              "Ethereum", "Pirl", "Nicehash-Lyra2REv2", "Vertcoin", "Nicehash-Ethash", "Monacoin", "GroestlCoin",
+              "Musicoin", "Expanse", "Orbitcoin", "Metaverse", "Komodo", "Ubiq", "LBRY", "Nicehash-Skunkhash",
+              "Electroneum", "Sibcoin", "Nicehash-X11Gost", "Nicehash-LBRY", "Karbowanec",  "Nicehash-CryptoNight",
+              "Decred", "Monero", "Sumokoin", "Nicehash-Blake (14r)", "PascalLite", "Halcyon","Bytecoin","Pascalcoin",
+              "Soilcoin", "Nicehash-Pascal", "Sia","Nicehash-Blake (2b)","DGB-Groestl", "Myriad-Groestl",  "DigitalNote"  });
+
+
+            foreach (var coin in coinsList)
+            {
+                int profit = (int)rss["coins"][coin]["profitability"];
+                coinProfit.Add(coin, profit);
+            }
+
+            var sortedCoinsProfitList = MergeSorter.MergeSort(coinProfit);
+
+            foreach (KeyValuePair<string, int> element in sortedCoinsProfitList)
+            {
+                Console.Write(element.Key);
+                Console.Write(" ");
+                Console.Write(element.Value);
+                Console.WriteLine(" ");
+            }
+
+            //   Console.WriteLine(coinProfit["EthereumClassic"]);
+            Console.ReadLine();
+
+        }
+
+
+        private static T _download_serialized_json_data<T>(string url) where T : new()
+        {
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }
+
+        private static string download_serialized_json_data2(string url)
+        {
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return json_data;
+            }
+        }
+
+    }
+}
